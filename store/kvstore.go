79c026245d2798c6830645df8306a0597fd3d62a
shimanekb <brandon.j.shimanek@gmail.com>
2021-02-15 17:50:12 -0600

    consolidated file

--- store/kvstore.go
+++ store/kvstore.go
@@ -8,47 +8,11 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 const (
-	STORAGE_DIR string = "storage"
-	A_FILE      string = "a_record.csv"
-	B_FILE      string = "b_record.csv"
-	C_FILE      string = "c_record.csv"
-	D_FILE      string = "d_record.csv"
-	E_FILE      string = "e_record.csv"
-	F_FILE      string = "f_record.csv"
-	G_FILE      string = "g_record.csv"
-	H_FILE      string = "h_record.csv"
-	I_FILE      string = "i_record.csv"
-	J_FILE      string = "j_record.csv"
-	K_FILE      string = "k_record.csv"
-	L_FILE      string = "l_record.csv"
-	M_FILE      string = "m_record.csv"
-	N_FILE      string = "n_record.csv"
-	O_FILE      string = "o_record.csv"
-	P_FILE      string = "p_record.csv"
-	Q_FILE      string = "q_record.csv"
-	R_FILE      string = "r_record.csv"
-	S_FILE      string = "s_record.csv"
-	T_FILE      string = "t_record.csv"
-	U_FILE      string = "u_record.csv"
-	V_FILE      string = "v_record.csv"
-	W_FILE      string = "w_record.csv"
-	X_FILE      string = "x_record.csv"
-	Y_FILE      string = "y_record.csv"
-	Z_FILE      string = "z_record.csv"
-	ZERO_FILE   string = "0_record.csv"
-	ONE_FILE    string = "1_record.csv"
-	TWO_FILE    string = "2_record.csv"
-	THREE_FILE  string = "3_record.csv"
-	FOUR_FILE   string = "4_record.csv"
-	FIVE_FILE   string = "5_record.csv"
-	SIX_FILE    string = "6_record.csv"
-	SEVEN_FILE  string = "7_record.csv"
-	EIGHT_FILE  string = "8_record.csv"
-	NINE_FILE   string = "9_record.csv"
+	STORAGE_DIR  string = "storage"
+	STORAGE_FILE string = "data_records.csv"
 )
 
 type Store interface {
@@ -58,20 +22,13 @@ type Store interface {
 }
 
 type KvStore struct {
-	Cache              Cache
-	LastLetterIndexMap map[string]string
-}
-
-func GetLastCharLower(key string) string {
-	newVal := key[len(key)-1:]
-	return strings.ToLower(newVal)
+	Cache Cache
 }
 
 func (k KvStore) Put(key string, value string) error {
 	k.Cache.Add(key, value)
-	lastChar := GetLastCharLower(key)
 	path := filepath.Join(".", STORAGE_DIR)
-	path = filepath.Join(path, k.LastLetterIndexMap[lastChar])
+	path = filepath.Join(path, STORAGE_FILE)
 	return WritePut(path, key, value)
 }
 
@@ -82,9 +39,8 @@ func (k KvStore) Get(key string) (value string, err error) {
 		value = val
 		err = nil
 	} else {
-		lastChar := GetLastCharLower(key)
 		path := filepath.Join(".", STORAGE_DIR)
-		path = filepath.Join(path, k.LastLetterIndexMap[lastChar])
+		path = filepath.Join(path, STORAGE_FILE)
 		value, err = ReadGet(path, key)
 	}
 
@@ -93,52 +49,9 @@ func (k KvStore) Get(key string) (value string, err error) {
 
 func (k KvStore) Del(key string) error {
 	k.Cache.Remove(key)
-	lastChar := GetLastCharLower(key)
 	path := filepath.Join(".", STORAGE_DIR)
-	path = filepath.Join(path, k.LastLetterIndexMap[lastChar])
+	path = filepath.Join(path, STORAGE_FILE)
 	return WriteDel(path, key)
-}
-
-func NewLastLetterIndexMap() map[string]string {
-	var m map[string]string = make(map[string]string)
-	m["a"] = A_FILE
-	m["b"] = B_FILE
-	m["c"] = C_FILE
-	m["d"] = D_FILE
-	m["e"] = E_FILE
-	m["f"] = F_FILE
-	m["g"] = G_FILE
-	m["h"] = H_FILE
-	m["i"] = I_FILE
-	m["j"] = J_FILE
-	m["k"] = K_FILE
-	m["l"] = L_FILE
-	m["m"] = M_FILE
-	m["n"] = N_FILE
-	m["o"] = O_FILE
-	m["p"] = P_FILE
-	m["q"] = Q_FILE
-	m["r"] = R_FILE
-	m["s"] = S_FILE
-	m["t"] = T_FILE
-	m["u"] = U_FILE
-	m["v"] = V_FILE
-	m["w"] = W_FILE
-	m["x"] = X_FILE
-	m["y"] = Y_FILE
-	m["z"] = Z_FILE
-	m["0"] = ZERO_FILE
-	m["1"] = ONE_FILE
-	m["2"] = TWO_FILE
-	m["3"] = THREE_FILE
-	m["4"] = FOUR_FILE
-	m["5"] = FIVE_FILE
-	m["6"] = SIX_FILE
-	m["7"] = SEVEN_FILE
-	m["8"] = EIGHT_FILE
-	m["9"] = NINE_FILE
-
-	return m
 }
 
 func NewKvStore() *KvStore {
@@ -159,8 +72,7 @@ func NewKvStore() *KvStore {
 		log.Fatal("Could not create cache for kv store.")
 	}
 
-	var m map[string]string = NewLastLetterIndexMap()
-	return &KvStore{cache, m}
+	return &KvStore{cache}
 }
 
 func WritePut(filePath string, key string, value string) error {
